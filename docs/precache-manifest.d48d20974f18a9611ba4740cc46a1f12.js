a8095930e0429d9f15a55d70be0d40993984dd83
Zababurin <s.zababurin.v@gmail.com>
2021-03-20 20:46:19 +0300

    add page

---
+++ docs/precache-manifest.d48d20974f18a9611ba4740cc46a1f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a83c11fda168af1e925a1aef068b57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0426012479f6d5ba0909",
+    "url": "/static/css/main.f579d04f.chunk.css"
+  },
+  {
+    "revision": "dc0026a6bb0f0772df13",
+    "url": "/static/js/2.91ca1387.chunk.js"
+  },
+  {
+    "revision": "f5068d881178a07b73f2f574ac0bcf94",
+    "url": "/static/js/2.91ca1387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0426012479f6d5ba0909",
+    "url": "/static/js/main.227e4e90.chunk.js"
+  },
+  {
+    "revision": "25dfa7cd0df0deecd153",
+    "url": "/static/js/runtime-main.b30d749f.js"
+  }
+]);
